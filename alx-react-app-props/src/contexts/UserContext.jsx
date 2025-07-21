27ec689f8ff70f70b3b3e078874207e645412738
Tabitha Margaret Wangechi <135862691+Tabbymargaret@users.noreply.github.com>
2025-07-21 10:28:37 +0000

    Update contexts folder with UserContext file

---
+++ alx-react-app-props/src/contexts/UserContext.jsx
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const UserContext = createContext(); // ✅ correctly create and export the context
+
+export default UserContext; // ✅ export the context for use in other components
